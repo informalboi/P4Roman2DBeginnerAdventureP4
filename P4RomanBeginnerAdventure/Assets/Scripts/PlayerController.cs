91cebda39c71f27b72da44193d88b7bc79bcfe50
informalboi <733416@student.dusd.net>
2025-11-04 11:40:29 -0800

    left off at read keyboard
    finished around left off read keyboard

--- P4RomanBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P4RomanBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called bbefore the first frame update
+    void start()
+    {
+
+
+    }
+    // update is called once per frame
     void Start()
     {
-        
-    }
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        Debug.Log(horizontal);
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
+
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log(vertical);
+        // Update is called once per frame
+        void Update()
+        {
+            Vector2 position = transform.position;
+            position.x = position.x + 0.1f * horizontal;
+            position.y = position.y + 0.1f * vertical;
+            transform.position = position;
+        }
     }
 }
+
