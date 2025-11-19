4569e7d6b6e68a5f206d7c8f5251f43e844d1848
informalboi <733416@student.dusd.net>
2025-11-19 10:21:42 -0800

    11/17
    Added more tiles

--- P4RomanBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P4RomanBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -5,36 +5,40 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called bbefore the first frame update
+    public InputAction LeftAction;
+    // Start is called before the first frame update
     void start()
     {
 
 
+
     }
     // update is called once per frame
-    void update()
+    void Update()
     {
         float horizontal = 0.0f;
         if (Keyboard.current.leftArrowKey.isPressed)
         {
-            horizontal = -1.0f;
-        }
+            horizontal = -0.2f;
+            }
         else if (Keyboard.current.rightArrowKey.isPressed)
         {
-            horizontal = 1.0f;
+            horizontal = 0.2f;
         }
         Debug.Log(horizontal);
 
         float vertical = 0.0f;
         if (Keyboard.current.upArrowKey.isPressed)
         {
-            vertical = 1.0f;
+            vertical = 0.2f;
         }
         else if (Keyboard.current.downArrowKey.isPressed)
         {
-            vertical = -1.0f;
+            vertical = -0.2f;
         }
         Debug.Log(vertical);
+
+
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
         position.y = position.y + 0.1f * vertical;
