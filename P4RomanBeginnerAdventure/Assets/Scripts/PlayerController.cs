e1064e9b0000ecc66cc1c8c11f71898b31d2ecad
informalboi <733416@student.dusd.net>
2025-11-21 11:43:38 -0800

    finished 2d vector movement

--- P4RomanBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P4RomanBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -2,46 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerController : MonoBehaviour
 {
-    public InputAction LeftAction;
+   
+    public InputAction MoveAction;
+
+
+    public float Horizontal { get; private set; }
+
     // Start is called before the first frame update
-    void start()
+    void Start()
     {
-        LeftAction.Enable();
+        MoveAction.Enable();
+        
 
 
     }
     // update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -0.2f;
-            }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 0.2f;
-        }
-        Debug.Log(horizontal);
-
-        float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 0.2f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -0.2f;
-        }
-        Debug.Log(vertical);
-
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.01f;
         transform.position = position;
     }
 }
